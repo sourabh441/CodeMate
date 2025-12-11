515fa054d3d638c41313e9af44eae30ff8b1b3cd
sourabh <sahilsahita441@gmail.com>
2025-12-12 01:38:12 +0530

    understanding the PUT POST GET DELETE  and playing around with them

--- src/app.js
+++ src/app.js
@@ -6,12 +6,29 @@ app.use("/hello",(req,res)=>{
     res.send("hello from the server");
 })
 
-app.use("/test",(req, res)=>{
-    res.send("test test teestttt!!")
+app.get("/test",(req, res)=>{
+    res.send({firstname:"test", lastName:"sourabh"})
+})
+
+app.post("/test",(req,res)=>{
+    // DB is created
+    res.send({firstName:"Sahil",LastName:"sahita"})
+})
+
+app.delete("/test",(req,res)=>{
+    res.send("data deleted successfully!")
+})
+
+app.put("/test",(req,res)=>{
+    res.send("data is updated and replaced sucessfully!")
 })
 
 app.get("/neighbour",(req, res)=>{
     res.send("the neighbour is saying hello to you sourabh");
+})
+
+app.patch("/test",(req,res)=>{
+    res.send("data is patched and bugs are fixed successfully")
 })
 
 
